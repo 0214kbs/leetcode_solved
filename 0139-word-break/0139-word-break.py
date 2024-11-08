5750e8e47203049e8cccf256362cfd9111f05dc1
곽보선 <87002218+0214kbs@users.noreply.github.com>
2024-11-08 16:24:29 +0900

    Time: 4 ms (41.24%), Space: 16.7 MB (36.22%) - LeetHub

--- 0139-word-break/0139-word-break.py
+++ 0139-word-break/0139-word-break.py
@@ -1,7 +1,10 @@
 class Solution:
 
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        check = {}
         def dfs(start):
+            if start in check:
+                return check[start]
             if start == len(s):
                 return True
 
@@ -9,5 +12,6 @@ class Solution:
                 if s[start:start+len(word)] == word:
                     if dfs(start+len(word)):
                         return True
+            check[start] =False
             return False
         return dfs(0)
