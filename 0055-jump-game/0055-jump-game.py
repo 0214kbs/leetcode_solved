22adf2bd0dfdf51edc109be9dcaae425c26299db
곽보선 <87002218+0214kbs@users.noreply.github.com>
2024-11-12 12:56:26 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -1,11 +1,9 @@
-import heapq
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
         if len(nums) == 1: return True
         maxv = 0
         for ni, num in enumerate(nums):
-            if ni > maxv:
-                return False
+            if ni > maxv: return False
             maxv = max(ni+num, maxv)
-            if maxv >= len(nums)-1: return True
+            if maxv > len(nums): return True
         return False
