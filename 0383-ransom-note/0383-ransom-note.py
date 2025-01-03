9350432dfe5e0b7263ffbf6154e0aed6aa99f1a9
곽보선 <87002218+0214kbs@users.noreply.github.com>
2025-01-03 13:47:04 +0900

    Time: 11 ms (89.04%), Space: 18.1 MB (8.7%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -1,13 +1,4 @@
+from collections import Counter
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        check = {}
-        for m in magazine:
-            if m in check:
-                check[m] +=1
-            else: check[m] = 1
-        for r in ransomNote:
-            if r in check:
-                check[r] -= 1
-                if check[r] < 0: return False
-            else: return False
-        return True
+        return not (Counter(ransomNote) - Counter(magazine))
